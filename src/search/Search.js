fb39308cc31c0bfd2a2b9aea03205b62a365668a
James <jfoshee2@students.kennesaw.edu>
2021-05-02 13:05:38 -0400

    Adds Search Bar

---
+++ src/search/Search.js
@@ -0,0 +1,45 @@
+﻿import React, {Component} from "react";
+import SearchIcon from '@material-ui/icons/Search';
+
+class Search extends Component {
+    
+    constructor(props) {
+        super(props);
+    }
+    
+    componentDidMount() {
+        
+    }
+    
+    handleChange = (event) => {
+        
+    }
+    
+    handleBlur = (event) => {
+        
+    }
+    
+    handleKeyDown = (event) => {
+        let characters = ['e', '-', '+', '=', '*', '/'];
+        if (characters.includes(event.key)) {
+            event.preventDefault();
+        }
+    }
+    
+    render() {
+        return(
+            <div>
+                <input type="number" 
+                       onChange={this.handleChange} 
+                       onKeyDown={this.handleKeyDown} 
+                       onBlur={this.handleBlur} 
+                />
+                <SearchIcon />
+            </div>
+        );
+        
+    }
+
+}
+
+export default Search;
